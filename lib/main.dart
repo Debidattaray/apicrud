87f760bf0852dfb91099a1999f00ff6d4fd186af
Debidatta <debidattaray@gmail.com>
2021-04-29 10:28:36 +0530

    post

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,87 @@
-import 'dart:convert';
+// import 'dart:convert';
 
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
 
-class _MyHomePageState extends State<MyHomePage> {
-  // Future<void>
-  Future<List<User>> getuserdata() async {
-    var response = await http.get(
-      Uri.https('jsonplaceholder.typicode.com', 'users'),
-    );
-    // print(response.body);
+// class _MyHomePageState extends State<MyHomePage> {
+//   // Future<void>
+//   Future<List<User>> getuserdata() async {
+//     var response = await http.get(
+//       Uri.https('jsonplaceholder.typicode.com', 'users'),
+//     );
+//     // print(response.body);
 
-    var jsonData = jsonDecode(response.body);
+//     var jsonData = jsonDecode(response.body);
 
-    List<User> users = [];
+//     List<User> users = [];
 
-    for (var u in jsonData) {
-      User user = User(u["name"], u["email"], u["username"]);
-      users.add(user);
-    }
-    print(users);
-    return users;
-  }
+//     for (var u in jsonData) {
+//       User user = User(u["name"], u["email"], u["username"]);
+//       users.add(user);
+//     }
+//     print(users);
+//     return users;
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Card(
-          child: FutureBuilder(
-            future: getuserdata(),
-            builder: (context, AsyncSnapshot snapshot) {
-              if (snapshot.data == null) {
-                return Container(
-                  child: Center(
-                    child: Text("Loading"),
-                  ),
-                );
-              } else
-                return ListView.builder(
-                    itemCount: snapshot.data.length,
-                    // itemCount: snapshot.data == null ? 0 : snapshot.data.length,
-                    itemBuilder: (context, i) {
-                      return ListTile(title: Text(snapshot.data[i].name)
-                          // title: Text(snapshot.data[i].n),
-                          );
-                    });
-            },
-          ),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Container(
+//         child: Card(
+//           child: FutureBuilder(
+//             future: getuserdata(),
+//             builder: (context, AsyncSnapshot snapshot) {
+//               if (snapshot.data == null) {
+//                 return Container(
+//                   child: Center(
+//                     child: Text("Loading"),
+//                   ),
+//                 );
+//               } else
+//                 return ListView.builder(
+//                     itemCount: snapshot.data.length,
+//                     // itemCount: snapshot.data == null ? 0 : snapshot.data.length,
+//                     itemBuilder: (context, i) {
+//                       return ListTile(title: Text(snapshot.data[i].name)
+//                           // title: Text(snapshot.data[i].n),
+//                           );
+//                     });
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
-class User {
-  final String name, email, username;
+// class User {
+//   final String name, email, username;
 
-  User(this.name, this.email, this.username);
-}
+//   User(this.name, this.email, this.username);
+// }
 
 // import 'dart:async';
 // import 'dart:convert';
@@ -168,3 +168,140 @@ class User {
 //     );
 //   }
 // }
+//
+//
+//
+//
+
+import 'dart:convert';
+// import 'dart:ffi';
+
+import 'package:apicrud/datamodel.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+// Future<Album> createAlbum(String title) async {
+//   final response = await http.post(
+//     Uri.https('jsonplaceholder.typicode.com', 'albums'),
+//     headers: <String, String>{
+//       'Content-Type': 'application/json; charset=UTF-8',
+//     },
+//     body: jsonEncode(<String, String>{
+//       'title': title,
+//     }),
+//   );
+//   if (response.statusCode == 201) {
+//     // If the server did return a 201 CREATED response,
+//     // then parse the JSON.
+//     return Album.fromJson(jsonDecode(response.body));
+//   } else {
+//     // If the server did not return a 201 CREATED response,
+//     // then throw an exception.
+//     throw Exception('Failed to load album');
+//   }
+// }
+
+Future<DataModel> submitData(String name, String job) async {
+  final response = await http.post(Uri.https("reqres.in", "api/users"),
+      // headers: <String, String>{
+      // 'Content-Type': 'application/json; charset=UTF-8',
+      // },
+      body: {'name': name, 'job': job});
+  var data = response.body;
+  print(data);
+
+  if (response.statusCode == 201) {
+    String responseString = response.body;
+    return dataModelFromJson(responseString);
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
+
+TextEditingController nameController = TextEditingController();
+TextEditingController jobController = TextEditingController();
+
+class _MyHomePageState extends State<MyHomePage> {
+  late DataModel _dataModel;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("API"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(4.0),
+                    ),
+                  ),
+                  hintText: "Enter Text",
+                ),
+                controller: nameController,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(4.0),
+                    ),
+                  ),
+                  hintText: "Enter job",
+                ),
+                controller: jobController,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  String name = nameController.text;
+                  String job = jobController.text;
+
+                  DataModel data = await submitData(name, job);
+
+                  setState(() {
+                    _dataModel = data;
+                  });
+                },
+                child: Text("Submit"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
